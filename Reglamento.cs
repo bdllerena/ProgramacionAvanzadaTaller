41f38813f40f062ddfcfe2254147485048a60b3e
davidshaek <davidshaek@gmail.com>
2017-10-18 13:31:28 -0500

    Reglamento codigo

---
+++ Reglamento.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EspePocket.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reglamento : ContentPage
+    {
+        public Reglamento()
+        {
+            Title = "Reglamentos";
+            var browser = new WebView
+            {
+
+                Source = "https://goo.gl/wjpD7L"
+            };
+            Content = browser;
+        }
+    }
+
+}
